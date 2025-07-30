42917b7c3e9417193a0dcd301d52d0df8db8dd69
Holison18 <kobbyholison2002@gmail.com>
2025-07-30 11:17:07 +0000

    ingredients update

--- src/data/products.ts
+++ src/data/products.ts
@@ -11,42 +11,42 @@ export const products: Product[] = [
     id: "1",
     name: "Renee's Shito (Big Size)",
     price: 25.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product1
   },
   {
     id: "2",
     name: "Renee's Shito (Medium Size)",
     price: 15.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product2
   },
   {
     id: "3",
     name: "Renee's Shito (Small Size)",
     price: 12.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product3
   },
   {
     id: "4",
     name: "Renee's Shito (Biggest Size)",
     price: 30.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product4
   },
   {
     id: "5",
     name: "Renee's Shito (Sachet)",
     price: 1.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product5
   },
   {
     id: "6",
     name: "Renee's Shito (Sachet)",
     price: 2.00,
-    description: "Pepper, Shrimps, onion, garlic and natural Ghanaian spices",
+    description: "Shrimp and fish powder, onion, pepper, ginger, garlic.",
     image: product6
   }
 ];
